d13a1925770c07ad1223be9fa3a414fe9a34f423
Shokoku <shokoku@outlook.kr>
2023-04-13 19:04:11 +0900

    5. 회원 삭제 기능 구현

--- src/main/java/org/example/groupware/controller/MemberController.java
+++ src/main/java/org/example/groupware/controller/MemberController.java
@@ -62,4 +62,10 @@ public class MemberController {
         model.addAttribute("member", memberDTO);
         return "member/detail";
     }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("id") Long id) {
+        memberService.delete(id);
+        return "redirect:/member/";
+    }
 }
