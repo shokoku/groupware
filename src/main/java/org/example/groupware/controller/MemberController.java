ab09942e28c7a64463aa6934248f72796e5504ad
Shokoku <shokoku@outlook.kr>
2023-04-13 19:36:08 +0900

    6. 회원 수정 기능 구현

--- src/main/java/org/example/groupware/controller/MemberController.java
+++ src/main/java/org/example/groupware/controller/MemberController.java
@@ -68,4 +68,23 @@ public class MemberController {
         memberService.delete(id);
         return "redirect:/member/";
     }
+
+    @GetMapping("/update")
+    public String updateForm(HttpSession session, Model model) {
+        // 세션에 저장된 나의 이메일 가져오기
+        String loginEmail = (String) session.getAttribute("loginEmail");
+        MemberDTO memberDTO = memberService.findByMemberEmail(loginEmail);
+        model.addAttribute("member", memberDTO);
+        return "member/update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute MemberDTO memberDTO) {
+        boolean result = memberService.update(memberDTO);
+        if (result) {
+            return "redirect:/member?id=" + memberDTO.getId();
+        } else {
+            return "index";
+        }
+    }
 }
